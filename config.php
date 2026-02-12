c082374400677365f06f2adc756b7058f2f0af9c
Grzegorz <grzegorzmitula@interia.pl>
2026-02-12 21:48:49 +0100

    clean moodle51 + fixes

---
+++ config.php
@@ -0,0 +1,32 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mariadb';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'moodle51';
+$CFG->dbuser    = 'root';
+$CFG->dbpass    = '';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => '',
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_unicode_ci',
+);
+
+$CFG->wwwroot   = 'http://moodle51.lan';
+$CFG->dataroot  = 'C:\\moodledatas\\moodle51';
+$CFG->admin     = 'admin';
+
+$CFG->debug = 32767;
+$CFG->debugdisplay = 1;
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
